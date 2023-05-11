695fb580d7c2cadaa17a59a13e897130b59cf660
samyak <samyak257@gmail.com>
2023-05-11 11:52:43 -0500

    made progress

---
+++ courseSchedule2.py
@@ -0,0 +1,43 @@
+from collections import deque
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # initialize the graph as adjacency list
+        source_nodes = set()
+        graph = defaultdict(list)
+        # graph = {}
+        for edge in prerequisites:
+            source_nodes.add(edge[0])
+            if edge[1] in source_nodes:
+                source_nodes.remove(edge[1])
+
+            graph[edge[0]] += [edge[1]]
+            graph[[edge[1]]] = []
+
+        
+        # this means there is a course that you can not get to
+        if numCourses != len(list(graph.keys())): 
+            return []
+        
+        # check for cycles
+        rets = {}
+        def dfs(stack, seen):
+            top = stack.pop()
+            if top in seen:
+                return False
+            for neighbor in graph[top]:
+                copy_seen, copy_stack = seen.copy(), stack.copy()
+                copy_seen.add(neighbor); copy_stack.add(neighbor)
+                ret = dfs(copy_stack, copy_seen)
+                del copy_seen, copy_stack
+                if ret == True:
+                    rets[top] = True
+                else:
+                    return False
+
+
+
+        
+
