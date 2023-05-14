e31ee92c1661328825c04cec7b094d4f0ede7e51
samyak <samyak257@gmail.com>
2023-05-13 20:04:41 -0500

    implemented top sort

--- topologicalSort.py
+++ topologicalSort.py
@@ -1,9 +1,11 @@
 # implementing top sort from scratch, using edges
-# assuming there are no cyclic graphs present
+# assuming there are no cyclic graphs present- edit no need, enforce a simple check prior
 # find node(s) with no incoming edges add them to list
 # graph data structure to allow easy removing
 
 # dict = {node: [[outgoing], [incoming]]}
+
+from typing import Dict
 
 def parse(edges):
     graph = {}
@@ -23,15 +25,47 @@ def parse(edges):
     return graph
     
 
+
 def topsort(graph):
     # find nodes with no incoming edges
     # add them to cur
     # remove those nodes from the graph
     # recursively call topsort for cur = (no incoming edges nodes) + (recursive call result)
+    # add another check to see if there are no nodes without any incoming edges then the graph is cyclic!!
+    if len(graph) == 0:
+        return []
     cur = []
-    return cur
+    cyclic = True
+    for node in list(graph.keys()):
+        if len(graph[node][1]) == 0: # length of incoming edges to this node
+            cyclic = False
+            cur.append(node)
+    if cyclic:
+        print(graph)
+        return False
+    
+    for node in cur:
+        # remove edges
+        outgoing_edges = graph[node][0]
+        for outgoing in outgoing_edges:
+            graph[outgoing][1].remove(node)
+        # remove from graph
+        graph.pop(node)
+    
+    # recursive call
+    ret = topsort(graph)
+    if ret == False:
+        return False
+    else:
+        return cur + ret 
 
 
 edges = [[1,0],[2,0],[3,1],[3,2]]
 graph = parse(edges)
+# topsort(graph)
 print(topsort(graph))
+# graph = {}
+# print(len(graph))
+# graph[1] = 0
+# graph.pop(1)
+# print(graph)
