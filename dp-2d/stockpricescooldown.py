f2a2f12ba5a71058a55349a74238a54e2f3d9b1f
samyak <samyak257@gmail.com>
2023-05-18 18:59:40 -0700

    initial attmept

---
+++ dp-2d/stockpricescooldown.py
@@ -0,0 +1,22 @@
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        cache = {}
+        def subproblem(index, buying):
+            if index >= len(prices):
+                return 0
+
+            if (index, buying) in cache:
+                return cache[(index, buying)]
+
+            hold = subproblem(index + 1, buying)
+            if buying:
+                buy = subproblem(index + 1, not buying) - prices[index]
+                cache[(index, buying)] = max(buy, hold)
+            else:
+                sell = subproblem(index + 2, not buying)
+                cache[(index, buying)] = max(sell, hold)
+            return cache[(index, buying)]
+
+        return subproblem(0, True)
+            
