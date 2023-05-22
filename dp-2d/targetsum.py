615de512991da2aaf7a3a944c5d6d1eb3d3bbb40
samyak <samyak257@gmail.com>
2023-05-21 21:30:55 -0700

    recursive subproblem, non edge cases complete

---
+++ dp-2d/targetsum.py
@@ -0,0 +1,15 @@
+from typing import *
+
+
+def findTargetSumWays(self, nums: List[int], target: int) -> int:
+    
+    def recursive_subproblem(index, cur_sum):
+
+        # besides edge cases
+        add = recursive_subproblem(index + 1, cur_sum + nums[index] )
+        subtract = recursive_subproblem(index + 1, cur_sum - nums[index])
+        res = sum(add, subtract)
+        return res
+    
+    
+    return 0
