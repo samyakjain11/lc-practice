ca8c4a0321d2f703f55aff489d644d24e1bb7225
samyak <samyak257@gmail.com>
2023-05-22 22:42:31 -0700

    implement cache

--- dp-2d/targetsum.py
+++ dp-2d/targetsum.py
@@ -2,7 +2,7 @@ from typing import *
 
 
 def findTargetSumWays(self, nums: List[int], target: int) -> int:
-    
+    cache = {}
     def recursive_subproblem(index, cur_sum):
 
         # besides edge cases
@@ -10,4 +10,6 @@ def findTargetSumWays(self, nums: List[int], target: int) -> int:
         subtract = recursive_subproblem(index + 1, cur_sum - nums[index])
         res = sum(add, subtract)
         return res
+    
+    
     return 0
