5747bb14667cc022ba2d0d5b384dbbf9f5fef36f
samyak <samyak257@gmail.com>
2023-07-05 22:49:05 -0700

    attmept 1

---
+++ sliding-window/anagram-finder.py
@@ -0,0 +1,19 @@
+def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(s) < len(p): return []
+        
+        res = []
+        letters_unfulfilled = list(p)
+        indices_for_fulfilled_letters = set()
+
+        l = 0
+        for r in range(len(s)):
+            if r - l + 1 > len(p):
+                if l in indices_for_fulfilled_letters:
+                    letters_unfulfilled.append(s[l])
+                l += 1
+            if s[r] in p and s[r] in letters_unfulfilled:
+                letters_unfulfilled.remove(s[r]) 
+                indices_for_fulfilled_letters.add(r)
+                if len(letters_unfulfilled) == 0:
+                    res.append(l)
+        return res
