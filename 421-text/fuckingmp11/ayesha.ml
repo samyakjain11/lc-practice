f111377e0c74bf2c74e9734317b63d91a0d9b181
samyak <samyak257@gmail.com>
2023-12-05 00:34:34 -0600

    fuck u

--- 421-text/fuckingmp11/ayesha.ml
+++ 421-text/fuckingmp11/ayesha.ml
@@ -34,11 +34,10 @@ let rec app_cont_to_value env k v =
 				| None -> Failed
 				| Some (k', env') -> app_cont_to_value env' k' v)
 		| FnContCPS (str, exp_cps) ->
-			Intermediate(ValueBinding(str,v)::env, exp_cps)
+      let valBinding = ValueBinding(str,v) in
+			Intermediate(valBinding :: env, exp_cps)
 		| ExnMatch k ->
-			(match v with
-				| IntVal a -> appExceptionHelper
-       env k a
+			(match v with IntVal a -> appExceptionHelper env k a
 				| _ -> Failed)
 
 let rec one_step_exp_cps_eval env exp_cps = 
@@ -48,41 +47,45 @@ let rec one_step_exp_cps_eval env exp_cps =
 			(match lookup_value env x with
 				| None -> Failed
 				| Some v -> app_cont_to_value env k v)
-    | MonOpAppCPS (k, mon_op, x, ke) -> apply_mon_op env k mon_op x ke
-    | BinOpAppCPS(k, bin_op, x, y, ke) -> apply_bin_op env k bin_op x y ke
-    | AppCPS (k, f, x, ke) -> apply_app_cps env k f x ke
-    | IfCPS(b, e1, e2) -> apply_if_cps env b e1 e2
-		| FunCPS (k, x, ek, e, p) -> app_cont_to_value env k (CPSClosureVal (x, ek, e, p, env))
-		| FixCPS (k, f, x, ek, e, p) -> app_cont_to_value env k (CPSRecClosureVal(f, x, ek, e, p, env))
+    | MonOpAppCPS (k, operation, x, epsilon) ->
+			(match lookup_value env x with 
+				| None -> Failed
+				| Some v -> 
+					(match monOpApply operation v with 
+						| Exn n -> appExceptionHelper env epsilon n
+						| Value v' -> app_cont_to_value env k v'))
+    | BinOpAppCPS(k, operation, x, y, epsilon) -> 
+      (match (lookup_value env x, lookup_value env y) with 
+      | (Some firstVal, Some secondVal) -> 
+          (match binOpApply operation firstVal secondVal with
+           | Exn n -> appExceptionHelper env epsilon n
+           | Value v' -> app_cont_to_value env k v')
+      | _ -> Failed)
+    | AppCPS (k, f, x, epsilon) -> applyApp env k f x epsilon
+    | IfCPS(e1, e2, e3) -> (match lookup_value env e1 with 
+      | Some (BoolVal true) -> Intermediate (env, e2)  
+      | Some (BoolVal false) -> Intermediate (env, e3)  
+      | _ -> Failed )
+		| FunCPS (k, x, ek, e, p) -> 
+      let closureVal  = CPSClosureVal (x, ek, e, p, env) in
+      app_cont_to_value env k closureVal
+		| FixCPS (k, f, x, ek, e, p) ->
+      let closureVal = (CPSRecClosureVal(f, x, ek, e, p, env)) in 
+      app_cont_to_value env k closureVal
 
-and apply_mon_op env k mon_op x ke = 
-  match lookup_value env x with 
-  | None -> Failed
-  | Some v -> 
-      (match monOpApply mon_op v with 
-       | Exn n -> appExceptionHelper
-       env ke n
-       | Value v' -> app_cont_to_value env k v')
-
-and apply_bin_op env k bin_op x y ke = 
-  match (lookup_value env x, lookup_value env y) with 
-  | (Some v1, Some v2) -> 
-      (match binOpApply bin_op v1 v2 with
-       | Exn n -> appExceptionHelper
-       env ke n
-       | Value v' -> app_cont_to_value env k v')
-  | _ -> Failed
-
-and apply_app_cps env k f x ke = 
+and applyApp env k f x epsilon = 
   match (lookup_value env f, lookup_value env x) with
   | (Some (CPSClosureVal (y, kk, ek, e, env')), Some v) -> 
-      Intermediate ((ValueBinding(y, v) :: ContBinding(kk, (k, env)) :: ExnContBinding(ek, (ke, env)) :: env'), e)
+      let valBinding = ValueBinding(y, v) in 
+      let contBinding = ContBinding(kk, (k, env)) in 
+      let exceptionBinding = ExnContBinding(ek, (epsilon, env)) in 
+      let cpsEnvEntryList = (valBinding :: contBinding :: exceptionBinding :: env') in 
+      Intermediate (cpsEnvEntryList, e)
   | (Some (CPSRecClosureVal(g, y, kk, ek, e, env')), Some v) -> 
-      Intermediate ((ValueBinding(y, v) :: ValueBinding(g, CPSRecClosureVal(g, y, kk, ek, e, env')) :: ContBinding(kk, (k, env)) :: ExnContBinding(ek, (ke, env)) :: env'), e)
+      let valBinding = ValueBinding(y, v) in 
+      let valBinding2 = ValueBinding(g, CPSRecClosureVal(g, y, kk, ek, e, env')) in 
+      let contBinding = ContBinding(kk, (k, env)) in 
+      let exceptionBinding = ExnContBinding(ek, (epsilon, env)) in 
+      let cpsEnvEntryList = (valBinding :: valBinding2 :: contBinding :: exceptionBinding :: env') in 
+      Intermediate (cpsEnvEntryList, e)
   | _ -> Failed
-
-and apply_if_cps env b e1 e2 = 
-  match lookup_value env b with 
-  | Some (BoolVal true) -> Intermediate (env, e1)  
-  | Some (BoolVal false) -> Intermediate (env, e2)  
-  | _ -> Failed 
