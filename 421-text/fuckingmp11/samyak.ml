3cdf93e6a592785a72f2af3584aa755e5be87e97
samyak <samyak257@gmail.com>
2023-12-04 23:49:46 -0600

    fuck you even more cs 421

--- 421-text/fuckingmp11/samyak.ml
+++ 421-text/fuckingmp11/samyak.ml
@@ -2,15 +2,6 @@
 
 open Common;;
 
-(* let rec app_cont_to_value env k v = raise (Failure "Not implemented yet")
-
-let rec one_step_exp_cps_eval env exp_cps = raise (Failure "Not implemented yet") *)
-
-(* File: mp6.ml *)
-
-(* open Mp6common;; *)
-
-
 let rec lookup_handler list_to_search key_to_find = 
   match list_to_search with 
   | [] -> None 
@@ -50,8 +41,6 @@ let rec app_cont_to_value env k v =
 				| IntVal a -> app_exn_handler_help env k a
 				| _ -> Failed)
 
-
-
 let rec one_step_exp_cps_eval env exp_cps = 
 	match exp_cps with
 		| ConstCPS (k, c) -> app_cont_to_value env k (const_to_val c)
@@ -59,41 +48,39 @@ let rec one_step_exp_cps_eval env exp_cps =
 			(match lookup_value env x with
 				| None -> Failed
 				| Some v -> app_cont_to_value env k v)
-		| MonOpAppCPS (k, mon_op, x, ke) ->
-			(match lookup_value env x with 
-				| None -> Failed
-				| Some v -> 
-					(match monOpApply mon_op v with 
-						| Exn n -> app_exn_handler_help env ke n
-						| Value v' -> app_cont_to_value env k v'))
-		| BinOpAppCPS(k, bin_op, x, y, ke) -> 
-			(match lookup_value env x with 
-				| None -> Failed
-				| Some v1 -> 
-					(match lookup_value env y with
-						| None -> Failed
-						| Some v2 -> 
-							(match binOpApply bin_op v1 v2 with
-								| Exn n -> app_exn_handler_help env ke n
-								| Value v' -> app_cont_to_value env k v')))
-		| IfCPS(b, e1, e2) ->
-			(match lookup_value env b with 
-				| None -> Failed
-				| Some (BoolVal true) -> Intermediate (env, e1)
-				| Some (BoolVal false) -> Intermediate (env, e2))
-		| FunCPS (k, x, ek, e, p) -> 
-			app_cont_to_value env k (CPSClosureVal (x, ek, e, p, env))
-		| FixCPS (k, f, x, ek, e, p) -> 
-			 app_cont_to_value env k (CPSRecClosureVal(f, x, ek, e, p, env))
-		| AppCPS (k, f, x, ke) ->
-			(match lookup_value env x with
-				| None -> Failed
-				| Some v -> 
-					(match lookup_value env f with 
-						| None -> Failed
-						| Some (CPSClosureVal (y, kk, ek, e, env')) -> 
-							Intermediate ((ValueBinding(y, v) :: ContBinding(kk, (k, env)) :: ExnContBinding(ek, (ke, env)) :: env'), e)
-					 	| Some (CPSRecClosureVal(g, y, kk, ek, e, env')) -> 
-							Intermediate ((ValueBinding(y, v) :: ValueBinding(g, CPSRecClosureVal(g, y, kk, ek, e, env')) :: ContBinding(kk, (k, env)) :: ExnContBinding(ek, (ke, env)) :: env'), e)
-					 		))
-    
+    | MonOpAppCPS (k, mon_op, x, ke) -> apply_mon_op env k mon_op x ke
+    | BinOpAppCPS(k, bin_op, x, y, ke) -> apply_bin_op env k bin_op x y ke
+    | AppCPS (k, f, x, ke) -> apply_app_cps env k f x ke
+    | IfCPS(b, e1, e2) -> apply_if_cps env b e1 e2
+		| FunCPS (k, x, ek, e, p) -> app_cont_to_value env k (CPSClosureVal (x, ek, e, p, env))
+		| FixCPS (k, f, x, ek, e, p) -> app_cont_to_value env k (CPSRecClosureVal(f, x, ek, e, p, env))
+
+and apply_mon_op env k mon_op x ke = 
+  match lookup_value env x with 
+  | None -> Failed
+  | Some v -> 
+      (match monOpApply mon_op v with 
+       | Exn n -> app_exn_handler_help env ke n
+       | Value v' -> app_cont_to_value env k v')
+
+and apply_bin_op env k bin_op x y ke = 
+  match (lookup_value env x, lookup_value env y) with 
+  | (Some v1, Some v2) -> 
+      (match binOpApply bin_op v1 v2 with
+       | Exn n -> app_exn_handler_help env ke n
+       | Value v' -> app_cont_to_value env k v')
+  | _ -> Failed
+
+and apply_app_cps env k f x ke = 
+  match (lookup_value env f, lookup_value env x) with
+  | (Some (CPSClosureVal (y, kk, ek, e, env')), Some v) -> 
+      Intermediate ((ValueBinding(y, v) :: ContBinding(kk, (k, env)) :: ExnContBinding(ek, (ke, env)) :: env'), e)
+  | (Some (CPSRecClosureVal(g, y, kk, ek, e, env')), Some v) -> 
+      Intermediate ((ValueBinding(y, v) :: ValueBinding(g, CPSRecClosureVal(g, y, kk, ek, e, env')) :: ContBinding(kk, (k, env)) :: ExnContBinding(ek, (ke, env)) :: env'), e)
+  | _ -> Failed
+
+and apply_if_cps env b e1 e2 = 
+  match lookup_value env b with 
+  | Some (BoolVal true) -> Intermediate (env, e1)  
+  | Some (BoolVal false) -> Intermediate (env, e2)  
+  | _ -> Failed 
