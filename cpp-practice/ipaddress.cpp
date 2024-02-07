21d360224889920f293830d15359c50ec15a5d93
samyak <samyak257@gmail.com>
2024-02-07 10:33:36 -0600

    init cpp

---
+++ cpp-practice/ipaddress.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+using namespace std;
+
+class Solution {
+
+public:
+    vector<string> fetchNextAddresses(string s, vector<string> constructed) {
+        
+    }
+
+    vector<string> restoreIpAddresses(string s) {
+        // four integers between 0-255
+        // backtracking
+
+    }
+};
